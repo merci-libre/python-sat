9c44d07c8bb4cd9a1c762b73fe8d4009ee7a30f9
withoutdishonor <westwardfishdme@gmail.com>
2025-11-20 17:44:01 -0600

    fix imports

--- sat/sat.py
+++ sat/sat.py
@@ -28,8 +28,8 @@ def start():
         from serveradmintool.modules import main
         from serveradmintool.modules import ansi
     except ImportError:
-        from .modules import main
-        from .modules import ansi
+        from modules import main
+        from modules import ansi
 
     try:
         main.run(name, __version__)

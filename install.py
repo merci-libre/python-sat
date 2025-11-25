eec446dd404c02e9a6dcd02dfc5163cc697d702b
withoutdishonor <westwardfishdme@gmail.com>
2025-11-24 20:48:19 -0600

    fixed linux install not caroting stdout to /dev/null

--- install.py
+++ install.py
@@ -363,7 +363,7 @@ class Linux():
             print("checking if python-sat already exists...\n")
             subprocess.check_call(
                 [sys.executable, "-m", "pip", "show", "serveradmintool"],
-                stdout=sys.path("/dev/null"))
+                stdout=sys.path("/dev/null")[0])
             # force reinstall
             if INSTALLFLAGS.force_reinstall:
                 Linux().full_install(self.command)
